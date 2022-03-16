309e0e632fa78e57cf9c9643763a1c84fd863a91
Prince Mwase <princefranklinemwase@gmail.com>
2022-03-16 15:14:47 +0200

    models field updates

--- ncapp/models/patient.py
+++ ncapp/models/patient.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from .location import Location
 from datetime import date
+from django.utils import timezone
+
 class Patient(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -12,6 +14,6 @@ class Patient(models.Model):
                 ('f' , 'Female')
             ]
     )
-    date_of_birth = models.DateField('Date of Birth', default=date.today() )
+    date_of_birth = models.DateField('Date of Birth', default=timezone.now )
     def __str__(self):
         return self.first_name + ' ' + self.last_name
